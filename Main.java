9352b8e8773cdb063d80d787761971768f489ca9
Chris Koch <kochcc25@uwgb.edu>
2020-07-15 21:53:56 +0000

    Basically the turn-in, need to make it all array https://stackoverflow.com/questions/16363547/how-to-declare-an-array-of-different-data-types

--- Main.java
+++ Main.java
@@ -19,8 +19,10 @@ import java.util.*;
 public class Main {
 public static void main(String[] args) {
 Scanner inputword = new Scanner(System.in);
+boolean userflung = false;
 
-System.out.println("YOU HAVE OPENED UP THE BRIDGE...I mean...PROGRAM OF DEATH! \n");
+while (userflung == false) {
+System.out.println("YOU HAVE OPENED UP THE BRIDGE...I mean...PROGRAM OF DEATH!");
 System.out.println("What is your name?");
 String userName = inputword.nextLine();
 //Do not need to change
@@ -29,32 +31,43 @@ System.out.println("\nWhat is your quest?");
 String thisisthequest = inputword.nextLine();
 //Do not need to change
 
-System.out.println("\nWhat is the capital of Assyria? (You don't need to use the funny symbols)");
+System.out.println("\nWhat is the capital of Assyria? (You don't need to use the funny symbols over vowels.)");
 String oneofafewcities = inputword.nextLine();
+if (!"Assur".equals(oneofafewcities) && !"Shubat-Enlil".equals(oneofafewcities) && !"Kalhu".equals(oneofafewcities) && !"Dur-Sharrukin".equals(oneofafewcities) && !"Nineveh".equals(oneofafewcities) && !"Harran".equals(oneofafewcities)) {
+userflung = true;
+break;
+}
 /*https://en.wikipedia.org/wiki/Assyria
 Any of these: Assur (aktushually Aššur), Shubat-Enlil, Kalhu, Dur-Sharrukin, Nineveh, Harran
 */
 
 System.out.println("\nWhat is the airspeed of the unlaiden European swallow in whole kilometers per hour?");
-Double europeanswallows = inputword.nextDouble();
+int europeanswallows = inputword.nextInt();
+if ( europeanswallows != 45) {
+userflung = true;
+break;
+}
 /*use https://www.quora.com/What-is-the-air-speed-velocity-of-an-unladened-sparrow
 28 mph is roughly 12.5 m/s...45 kph*/
 
+
 System.out.println("\nWhat is the airspeed of the unladen African swallow in whole kilometers per hour?");
-Double africanswallows = inputword.nextDouble(); 
+int africanswallows = inputword.nextInt();
+if ( africanswallows != 34 ) {
+userflung = true;
+break;
+}
 /*use https://www.quora.com/What-is-the-air-speed-velocity-of-an-unladened-sparrow
 21 mph...33.8 kph*/
 
 
-
-
-
-/*  *******************CHANGE BELOW*********************
-System.out.println("\n\n\n\n\n\nSir " + userName + ", trying to " + thisisthequest + ",");
-System.out.println("Who does not realize " + oneofafewcities + " is the wrong way to spell the capitol of Assyria,");
-System.out.println("who assumes " + europeanswallows + "is the airspeed of the unlaiden European swallow,");
-System.out.println("and that the unladen airspeed of the African swallow is" + africanswallows + ",");
-System.out.println("has the program's fullest permission to do the thing!");*/
+if (userflung != true){
+System.out.println("Oh, you may go, enjoy your day.");
+}
+} //end of while loop
+if (userflung == true){
+System.out.println("[Laughs in evil] You are no king, YOU FAIL! GOOD DAY SIR OR MADAM!");
+}
 
 }
 }
